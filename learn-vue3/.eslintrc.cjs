79511e14248a9ab6decd9e3e1ec256a46783fb9d
ksm <rkdtjd0909@gmail.com>
2024-03-21 21:41:39 +0900

    기초 세팅
    - eslint세팅은 vscode글로벌 세팅까지 건드려서 일단 안함

--- learn-vue3/.eslintrc.cjs
+++ learn-vue3/.eslintrc.cjs
@@ -3,12 +3,25 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    // "prettier/prettier": ['error', {
+    //   singleQuate: true,
+    //   semi: true,
+    //   useTabs: true,
+    //   tabWidth: 2,
+    //   trailingComma: 'all',
+    //   printWidth: 80,
+    //   bracketSpacing: true,
+    //   arrowParens: 'avoid',
+    // }]
   }
 }
