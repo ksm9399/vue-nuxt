94677146e3d87410178fe72d0a96540d34a88951
ksm <rkdtjd0909@gmail.com>
2024-03-30 22:02:28 +0900

    slot 기본 사용법

---
+++ learn-vue3/src/components/slot/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <FancyButton>
+      버튼명 콘텐츠를 넘겨봄
+    </FancyButton>
+  </div>
+</template>
+
+<script setup>
+import FancyButton from '@/components/slot/FancyButton.vue'
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ learn-vue3/src/components/slot/FancyButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="fancy-btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
