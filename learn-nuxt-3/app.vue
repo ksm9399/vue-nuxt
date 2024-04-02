1ab841295217380ab704e95e90cf9fe303153224
ksm <rkdtjd0909@gmail.com>
2024-04-02 14:59:03 +0900

    NuxtPage 내장 컴포넌트

--- learn-nuxt-3/app.vue
+++ learn-nuxt-3/app.vue
@@ -1,6 +1,10 @@
 <template>
   <NuxtLayout>
+    <!-- NuxtPage의 경우 path의 변경이 일어나면 리랜더링 -->
     <NuxtPage />
+
+    <!-- 라우터뷰의 경우 컴포넌트가 마운트 될때 한번만 랜더링됨 -->
+    <!-- <RouterView /> -->
   </NuxtLayout>
 </template>
 
--- learn-nuxt-3/pages/course/[courseSlug].vue
+++ learn-nuxt-3/pages/course/[courseSlug].vue
@@ -45,6 +45,31 @@
       <p class="q-mt-lg text-grey-8">
         {{ course?.content }}
       </p>
+      <template #footer>
+        <q-btn
+          v-if="prevCourse"
+          label="이전 강의"
+          color="primary"
+          unelevated
+          :to="prevCourse.path"
+        />
+
+        <q-btn
+          label="쿼리 추가"
+          color="dark"
+          unelevated
+          :to="{ path: $route.path, query: { tiemstamp: Date.now() } }"
+        />
+
+        <q-space />
+        <q-btn
+          v-if="nextCourse"
+          label="다음 강의"
+          color="primary"
+          unelevated
+          :to="nextCourse.path"
+        />
+      </template>
     </AppCard>
   </div>
 </template>
@@ -52,7 +77,13 @@
 <script setup lang="ts">
 const route = useRoute();
 const courseSlug = route.params.courseSlug as string;
-const { course } = useCourse(courseSlug);
+const { course, prevCourse, nextCourse } = useCourse(courseSlug);
+
+console.log(`[courseSlug].vue 컴포넌트 setup hooks`);
+
+definePageMeta({
+  key: (route) => route.fullPath,
+});
 </script>
 
 <style scoped></style>
