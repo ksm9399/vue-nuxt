5fa7f9bd879c2d5dbafb4cf11e2d11781a35e516
ksm <rkdtjd0909@gmail.com>
2024-04-05 16:28:17 +0900

    NuxtLink 내장 컴포넌트 기능 테스트

--- learn-nuxt-3/pages/about.vue
+++ learn-nuxt-3/pages/about.vue
@@ -5,8 +5,23 @@
       <p class="q-mt-sm text-grey-8">
         Vue & Nuxt Mastery Class 웹은 Nuxt3로 만들어졌습니다.
       </p>
+      <div class="column">
+        <RouterLink to="/">Route Link Home</RouterLink>
+        <RouterLink to="https://youtube.com/@gymcoding">
+          Route Link Youtube
+        </RouterLink>
+        <br />
+        <NuxtLink prefetched-class="bg-yellow" :prefetch="false" to="/"
+          >NuxtLink Link Home</NuxtLink
+        >
+        <NuxtLink to="https://youtube.com/@gymcoding">
+          NuxtLink Link Youtube
+        </NuxtLink>
+      </div>
     </div>
   </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { RouterLink } from 'vue-router';
+</script>
