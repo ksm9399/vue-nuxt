e4b510d1bae5e17d7c387fa1b0d84fa117682006
ksm <rkdtjd0909@gmail.com>
2024-04-08 22:56:11 +0900

    레이아웃 구성 실습

---
+++ learn-nuxt-3/pages/admin/dynamic-layout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-my-xl text-center q-gutter-md">
+      <q-btn
+        label="default layout"
+        color="dark"
+        @click="changePageLayout('default')"
+      />
+      <q-btn
+        label="admin layout"
+        color="primary"
+        @click="changePageLayout('admin')"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { LayoutKey } from '#build/types/layouts';
+
+const changePageLayout = (layout: LayoutKey) => {
+  setPageLayout(layout); // 레이아웃 명을 넣으면 동적으로 변환 가능 - setPageLayout() = Nuxt제공 utils
+};
+
+definePageMeta({
+  layout: false,
+});
+</script>
+
+<style scoped></style>
